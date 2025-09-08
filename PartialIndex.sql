ca4400f104bc41f2923785e130a6cfbebaf0f435
Eddie <Eddie000321@outlook.com>
2025-09-07 20:41:04 -0400

    make read me

--- PartialIndex.sql
+++ PartialIndex.sql
@@ -1,4 +1,5 @@
-CREATE INDEX "recents" ON "moives" ("title") WHERE "year" = 2023;
+DROP INDEX IF EXISTS "recents";
+CREATE INDEX "recents" ON "movies" ("title") WHERE "year" = 2023;
 
 EXPLAIN QUERY PLAN
 SELECT "title" FROM "movies" WHERE "year" = 2023; 
